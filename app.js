ef6c2718d004670faab9ab56502bff6cab5d0a93
gabrielgog <gabrielgog96@gmail.com>
2021-08-30 15:33:05 +0100

    make responsive

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-//   setTimeout(() => {
-//     window.location.href = "home.html";
-//   }, 5000);
+  setTimeout(() => {
+    window.location.href = "home.html";
+  }, 5000);
 
   const logo = document.getElementById("logo");
 
